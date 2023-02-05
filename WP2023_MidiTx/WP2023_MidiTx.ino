480c090519a871cfc10784159687e63170b743d1
mnierz <mnierz@indy.rr.com>
2023-02-05 15:50:23 -0500

    State of code when MIDI equipment was delivered on 2/4/2023

--- WP2023_MidiTx/WP2023_MidiTx.ino
+++ WP2023_MidiTx/WP2023_MidiTx.ino
@@ -1,6 +1,20 @@
 #include <MIDI.h>
 #include <esp_now.h>
 #include <WiFi.h>
+#include <FastLED.h>
+
+// This section should be common to both the receiver and transmitter
+enum ledCommand_e : byte
+{
+  SOLID_COLOR = 1,
+  CHUNKY,
+};
+typedef struct ledCommand_struct
+{
+  byte command; // TODO consider adding blend speed
+  byte data[16]; // TODO try using a union
+};
+// End common section
 
 // Adafruit makes a MIDI board that can work with the ESP32. https://www.adafruit.com/product/4740
 MIDI_CREATE_INSTANCE(HardwareSerial, Serial2, MIDI); // Need to use different serial port for MIDI to avoid conflict with serial debug printing
@@ -12,6 +26,12 @@ typedef struct midi_struct {
   byte velocity;
 } midi_struct;
 
+const ledCommand_struct neutralPalette = { CHUNKY, { 0, 0xE9, 0xE9, 0xA0, 81, 0x40, 0x40, 0x40, 165, 0xFF, 0xFF, 0xB0, 255, 0xE9, 0xE9, 0xA0 } };
+const ledCommand_struct happyPalette = { CHUNKY, { 0, 0xFF, 0x99, 0x00, 130, 0xFF, 0xB0, 0x00, 220, 0x80, 0x4D, 0x00, 255, 0xFF, 0x99, 0x00 } };
+const ledCommand_struct sadPalette = { CHUNKY, { 0, 0x00, 0x00, 0xFF, 81, 0x30, 0x30, 0xFA, 165, 0x00, 0x00, 0x93, 255, 0x00, 0x00, 0xFF } };
+const ledCommand_struct angryPalette = { CHUNKY, { 0, 0xFF, 0x00, 0x00, 120, 0xFF, 0x20, 0x00, 125, 0xC0, 0x00, 0x00, 255, 0xFF, 0x00, 0x00 } };
+const ledCommand_struct solidRed = { SOLID_COLOR, {0xFF, 0x00, 0x00} };
+
 // This function will be automatically called when a NoteOn is received.
 // It must be a void-returning function with the correct parameters,
 // see documentation here:
@@ -19,13 +39,66 @@ typedef struct midi_struct {
 
 void handleNoteOn(byte channel, byte pitch, byte velocity)
 {
+  bool broadcastCommand = true;
+  ledCommand_struct ledCommand;
+  switch(pitch)
+  {
+    case 48:
+      ledCommand = neutralPalette;
+      break;
+    case 49:
+      ledCommand = happyPalette;
+      break;
+    case 50:
+      ledCommand = sadPalette;
+      break;
+    case 51:
+      ledCommand = angryPalette;
+      break;
+    case 60:
+      ledCommand.command = SOLID_COLOR;
+      ledCommand.data[0] = 0xE9;
+      ledCommand.data[1] = 0xE9;
+      ledCommand.data[2] = 0xA0;
+      break;
+    case 61:
+      ledCommand.command = SOLID_COLOR;
+      ledCommand.data[0] = 0xFF;
+      ledCommand.data[1] = 0x99;
+      ledCommand.data[2] = 0;
+      break;
+    case 62:
+      ledCommand.command = SOLID_COLOR;
+      ledCommand.data[0] = 0;
+      ledCommand.data[1] = 0;
+      ledCommand.data[2] = 0xFF;
+      break;
+    case 63:
+      ledCommand.command = SOLID_COLOR;
+      ledCommand.data[0] = 0xFF;
+      ledCommand.data[1] = 0;
+      ledCommand.data[2] = 0;
+      break;
+    default:
+      broadcastCommand = false;
+      break;
+  }
+  Serial.println(ledCommand.data[0]);
+  Serial.println(ledCommand.data[1]);
+  Serial.println(ledCommand.data[2]);
+  Serial.println(ledCommand.data[3]); 
+  if (broadcastCommand)
+  {
+    esp_err_t result = esp_now_send(0, (uint8_t *) &ledCommand, sizeof(ledCommand));
+    broadcastCommand = false;
+  }
   // Do whatever you want when a note is pressed.
-  midi_struct midiData;
-  midiData.midiCommand = 0x90;
-  midiData.channel = channel;
-  midiData.pitch = pitch;
-  midiData.velocity = velocity;
-  esp_err_t result = esp_now_send(0, (uint8_t *) &midiData, sizeof(midiData));
+//  midi_struct midiData;
+//  midiData.midiCommand = 0x90;
+//  midiData.channel = channel;
+//  midiData.pitch = pitch;
+//  midiData.velocity = velocity;
+//  esp_err_t result = esp_now_send(0, (uint8_t *) &midiData, sizeof(midiData));
   // TODO handle error case
   
   // Try to keep your callbacks short (no delays ect)
@@ -55,6 +128,49 @@ void handleNoteOff(byte channel, byte pitch, byte velocity)
 #endif
 }
 
+void handlePitchBend(byte channel, int bend)
+{
+  char buf[100];
+  sprintf(buf, "Pitch Bend: Channel: %d, Bend: %d", channel, bend);
+  Serial.println(buf);
+}
+uint8_t broadcastAddressx[] = {0x40, 0x22, 0xD8, 0x05, 0x52, 0x78};
+
+
+void handleControlChange(byte channel, byte data1, byte data2)
+{
+  bool broadcastCommand = true;
+  ledCommand_struct ledCommand;
+  switch (data1)
+  {
+    case 7:
+    {
+      ledCommand.command = 1;
+      CHSV hsv(data2 * 2, 255, 255);
+      CRGB rgb;
+      hsv2rgb_rainbow(hsv, rgb);
+      ledCommand.data[0] = rgb.r;
+      ledCommand.data[1] = rgb.g;
+      ledCommand.data[2] = rgb.b;
+    }
+    break;
+    default:
+    broadcastCommand = false;
+    break;
+  }
+  
+  if (broadcastCommand)
+  {
+    esp_err_t result = esp_now_send(broadcastAddressx, (uint8_t *) &ledCommand, sizeof(ledCommand));
+    broadcastCommand = false;
+  }
+
+  char buf[100];
+  sprintf(buf, "Control Change: Channel: %d, data1: %d, data2: %d", channel, data1, data2);
+  Serial.println(buf);
+}
+
+
 // -----------------------------------------------------------------------------
 
 /*********
@@ -70,7 +186,7 @@ void handleNoteOff(byte channel, byte pitch, byte velocity)
 
 // REPLACE WITH YOUR ESP RECEIVER'S MAC ADDRESS
 uint8_t broadcastAddress1[] = {0x40, 0x22, 0xD8, 0x02, 0x82, 0x24}; // Ring A
-uint8_t broadcastAddress2[] = {0x40, 0x22, 0xD8, 0x09, 0x5C, 0x28}; // Ring B
+uint8_t broadcastAddress2[] = {0x40, 0x22, 0xD8, 0x05, 0x52, 0x78}; // Ring B
 uint8_t broadcastAddress3[] = {0x40, 0x22, 0xD8, 0x03, 0xC3, 0xE0}; // Ring C
 uint8_t broadcastAddress4[] = {0x94, 0xE6, 0x86, 0x3D, 0x59, 0xB8}; // Mark's receiver
 
@@ -123,11 +239,18 @@ void setup() {
     Serial.println("Failed to add peer");
     return;
   }
+  // register second peer  
+  memcpy(peerInfo.peer_addr, broadcastAddress4, 6);
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+   }
   
   // Connect the handleNoteOn function to the library,
   // so it is called upon reception of a NoteOn.
   MIDI.setHandleNoteOn(handleNoteOn);  // Put only the name of the function
-
+//  MIDI.setHandlePitchBend(handlePitchBend);
+  MIDI.setHandleControlChange(handleControlChange);
   // Do the same for NoteOffs
   MIDI.setHandleNoteOff(handleNoteOff);
 
@@ -137,7 +260,12 @@ void setup() {
  
 void loop() {
   // Call MIDI.read the fastest you can for real-time performance.
-  MIDI.read();
+  if ( MIDI.read())
+  {
+    int b1 = MIDI.getType();
+    Serial.print("Type: ");
+    Serial.print(b1);
+  }
 
   // There is no need to check if there are messages incoming
   // if they are bound to a Callback function.
--- WP2023_RingRx/WP2023_RingRx.ino
+++ WP2023_RingRx/WP2023_RingRx.ino
@@ -13,6 +13,21 @@
 #include <WiFi.h>
 #include <FastLED.h>
 
+// This section should be common to both the receiver and transmitter
+enum ledCommand_e : byte
+{
+  SOLID_COLOR = 1,
+  CHUNKY,
+};
+
+typedef struct ledCommand_struct
+{
+  byte command;
+  byte data[16];
+};
+// End common section
+
+//#define TEST_AT_HOME
 
 #define NUM_LEDS 300
 
@@ -21,6 +36,7 @@
 
 #define HIGH_BRIGHTNESS 255
 #define LOW_BRIGHTNESS 10
+
 enum mode_e
 {
   SHOW = 0,
@@ -29,188 +45,35 @@ enum mode_e
 };
 mode_e displayMode = PRACTICE;
 
-uint32_t colors[] = {
-        CRGB::AliceBlue,
-        CRGB::Amethyst,
-        CRGB::AntiqueWhite,
-        CRGB::Aqua,
-        CRGB::Aquamarine,
-        CRGB::Azure,
-        CRGB::Beige,
-        CRGB::Bisque,
-        CRGB::Black,
-        CRGB::BlanchedAlmond,
-        CRGB::Blue,
-        CRGB::BlueViolet,
-        CRGB::Brown,
-        CRGB::BurlyWood,
-        CRGB::CadetBlue,
-        CRGB::Chartreuse,
-        CRGB::Chocolate,
-        CRGB::Coral,
-        CRGB::CornflowerBlue,
-        CRGB::Cornsilk,
-        CRGB::Crimson,
-        CRGB::Cyan,
-        CRGB::DarkBlue,
-        CRGB::DarkCyan,
-        CRGB::DarkGoldenrod,
-        CRGB::DarkGray,
-        CRGB::DarkGreen,
-        CRGB::DarkKhaki,
-        CRGB::DarkMagenta,
-        CRGB::DarkOliveGreen,
-        CRGB::DarkOrange,
-        CRGB::DarkOrchid,
-        CRGB::DarkRed,
-        CRGB::DarkSalmon,
-        CRGB::DarkSeaGreen,
-        CRGB::DarkSlateBlue,
-        CRGB::DarkSlateGray,
-        CRGB::DarkTurquoise,
-        CRGB::DarkViolet,
-        CRGB::DeepPink,
-        CRGB::DeepSkyBlue,
-        CRGB::DimGray,
-        CRGB::DodgerBlue,
-        CRGB::FireBrick,
-        CRGB::FloralWhite,
-        CRGB::ForestGreen,
-        CRGB::Fuchsia,
-        CRGB::Gainsboro,
-        CRGB::GhostWhite,
-        CRGB::Gold,
-        CRGB::Goldenrod,
-        CRGB::Gray,
-        CRGB::Green,
-        CRGB::GreenYellow,
-        CRGB::Honeydew,
-        CRGB::HotPink,
-        CRGB::IndianRed,
-        CRGB::Indigo,
-        CRGB::Ivory,
-        CRGB::Khaki,
-        CRGB::Lavender,
-        CRGB::LavenderBlush,
-        CRGB::LawnGreen,
-        CRGB::LemonChiffon,
-        CRGB::LightBlue,
-        CRGB::LightCoral,
-        CRGB::LightCyan,
-        CRGB::LightGoldenrodYellow,
-        CRGB::LightGreen,
-        CRGB::LightGrey,
-        CRGB::LightPink,
-        CRGB::LightSalmon,
-        CRGB::LightSeaGreen,
-        CRGB::LightSkyBlue,
-        CRGB::LightSlateGray,
-        CRGB::LightSteelBlue,
-        CRGB::LightYellow,
-        CRGB::Lime,
-        CRGB::LimeGreen,
-        CRGB::Linen,
-        CRGB::Magenta,
-        CRGB::Maroon,
-        CRGB::MediumAquamarine,
-        CRGB::MediumBlue,
-        CRGB::MediumOrchid,
-        CRGB::MediumPurple,
-        CRGB::MediumSeaGreen,
-        CRGB::MediumSlateBlue,
-        CRGB::MediumSpringGreen,
-        CRGB::MediumTurquoise,
-        CRGB::MediumVioletRed,
-        CRGB::MidnightBlue,
-        CRGB::MintCream,
-        CRGB::MistyRose,
-        CRGB::Moccasin,
-        CRGB::NavajoWhite,
-        CRGB::Navy,
-        CRGB::OldLace,
-        CRGB::Olive,
-        CRGB::OliveDrab,
-        CRGB::Orange,
-        CRGB::OrangeRed,
-        CRGB::Orchid,
-        CRGB::PaleGoldenrod,
-        CRGB::PaleGreen,
-        CRGB::PaleTurquoise,
-        CRGB::PaleVioletRed,
-        CRGB::PapayaWhip,
-        CRGB::PeachPuff,
-        CRGB::Peru,
-        CRGB::Pink,
-        CRGB::Plaid,
-        CRGB::Plum,
-        CRGB::PowderBlue,
-        CRGB::Purple,
-        CRGB::Red,
-        CRGB::RosyBrown,
-        CRGB::RoyalBlue,
-        CRGB::SaddleBrown,
-        CRGB::Salmon,
-        CRGB::SandyBrown,
-        CRGB::SeaGreen,
-        CRGB::Seashell,
-        CRGB::Sienna,
-        CRGB::Silver,
-        CRGB::SkyBlue,
-        CRGB::SlateBlue,
-        CRGB::SlateGray,
-        CRGB::Snow,
-        CRGB::SpringGreen,
-        CRGB::SteelBlue,
-        CRGB::Tan,
-        CRGB::Teal,
-        CRGB::Thistle,
-        CRGB::Tomato,
-        CRGB::Turquoise,
-        CRGB::Violet,
-        CRGB::Wheat,
-        CRGB::White,
-        CRGB::WhiteSmoke,
-        CRGB::Yellow,
-        CRGB::YellowGreen
-    };
-int NumColors = sizeof(colors) / sizeof(uint32_t);
+uint8_t colorIndex[NUM_LEDS];
+uint8_t whichPalette = 0;
+
+DEFINE_GRADIENT_PALETTE( black_gp ) { 
+    0,    0x00, 0x00, 0x00,
+  255,    0x00, 0x00, 0x00,
+};
+
+CRGBPalette16 currentPalette(black_gp);
+CRGBPalette16 targetPalette(black_gp);
 
 // This is an array of leds.  One item for each led in your strip.
 CRGB leds[NUM_LEDS];
 
-typedef struct midi_struct {
-  byte midiCommand;
-  byte channel;
-  byte pitch;
-  byte velocity;
-} midi_struct;
-
+bool newCommandReceived = false;
+ledCommand_struct ledCommand;
 
 //callback function that will be executed when data is received
-void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
-  midi_struct midiData;
-  if (len >= sizeof(midiData))
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len)
+{
+  if (len <= sizeof(ledCommand_struct))
   {
-    memcpy(&midiData, incomingData, sizeof(midiData));
-    // Middle C is pitch 60 so put into center of LED strip and make sure it is on the strip
-    int ledIndex = midiData.pitch - 30;
-    ledIndex = max(ledIndex, 0);
-    ledIndex = min(ledIndex, NumColors - 1);
-    uint32_t color;
-    if (midiData.midiCommand == 0x90) // NoteOn
-    {
-      fill_solid(leds, NUM_LEDS, colors[ledIndex]);
-      FastLED.show();
-    }
+    memcpy(&ledCommand, incomingData, sizeof(ledCommand_struct));
+    newCommandReceived = true;
   }
-
-  Serial.print("Bytes received: ");
-  Serial.println(len);
-  Serial.print("pitch: ");
-  Serial.println(midiData.pitch);
-  Serial.print("command: ");
-  Serial.println(midiData.midiCommand);
-  Serial.println();
+  else
+  {
+    Serial.println("OnDataRecv: Packet too short");
+  }
 }
  
 void setup() {
@@ -220,23 +83,39 @@ void setup() {
   // Use pins 16 and 17 for toggle switch
   pinMode(PRACTICE_MODE_PIN, INPUT_PULLUP);
   pinMode(TEST_MODE_PIN, INPUT_PULLUP);
-  
-  
+    
   // sanity check delay - allows reprogramming if accidently blowing power w/leds
   delay(2000);
+#ifdef TEST_AT_HOME
+  FastLED.addLeds<WS2812B, 23, GRB>(leds, NUM_LEDS);
+  FastLED.addLeds<WS2812B, 25, GRB>(leds, NUM_LEDS);
+  FastLED.addLeds<WS2812B, 26, GRB>(leds, NUM_LEDS);
+  FastLED.addLeds<WS2812B, 27, GRB>(leds, NUM_LEDS);
+  FastLED.addLeds<WS2812B, 32, GRB>(leds, NUM_LEDS);
+#else
   FastLED.addLeds<WS2812B, 23, RGB>(leds, NUM_LEDS);
   FastLED.addLeds<WS2812B, 25, RGB>(leds, NUM_LEDS);
   FastLED.addLeds<WS2812B, 26, RGB>(leds, NUM_LEDS);
   FastLED.addLeds<WS2812B, 27, RGB>(leds, NUM_LEDS);
   FastLED.addLeds<WS2812B, 32, RGB>(leds, NUM_LEDS);
+#endif
   FastLED.setBrightness(LOW_BRIGHTNESS);
   fill_solid(leds, NUM_LEDS, CRGB::Black); // Want to make sure all the LEDs are off before starting the radio
   FastLED.show();
 
+  uint8_t chunk = 10; // make sure length is a multiple of chunk or trap it later
+  for (int i = 0; i < NUM_LEDS; i+=chunk) {
+    uint8_t color = random8();
+    for (int j = 0; j < chunk; j++)
+    {
+      colorIndex[i + j] = color;
+    }
+  }
+  
   delay(1000); // Wait a bit for the power to settle before starting the radio
+  
   //Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
-
   //Init ESP-NOW
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
@@ -245,6 +124,7 @@ void setup() {
   // Once ESPNow is successfully Init, we will register for recv CB to
   // get recv packer info
   esp_now_register_recv_cb(OnDataRecv);
+//  pinMode(22, OUTPUT);
 }
  
 void loop() {
@@ -253,6 +133,28 @@ void loop() {
   {
     testPattern();
   }
+
+  if (newCommandReceived)
+  {
+    newCommandReceived = false;
+    processNewCommand();
+  }
+  
+  for (int i = 0; i < NUM_LEDS; i++)
+  {
+    leds[i] = ColorFromPalette(currentPalette, colorIndex[i]);
+  }
+  nblendPaletteTowardPalette(currentPalette, targetPalette, 80);
+  EVERY_N_MILLISECONDS(20)
+  {
+    for (int i = 0; i < NUM_LEDS; i++)
+    {
+      colorIndex[i] += 2;
+    }
+  }
+//  digitalWrite(22, HIGH);
+  FastLED.show(); // 19 msec with V3.5 and 9 msec with 3.3.3
+//  digitalWrite(22, LOW);
 }
 
 void checkToggleSwitch(void)
@@ -273,18 +175,64 @@ void checkToggleSwitch(void)
     {
       case SHOW:
         FastLED.setBrightness(HIGH_BRIGHTNESS);
-      break;
+        break;
       case PRACTICE:
         FastLED.setBrightness(LOW_BRIGHTNESS);
-      break;
+        break;
       case TEST:
         FastLED.setBrightness(LOW_BRIGHTNESS);
-      break;
+        break;
       default: // This should never happen but it's good coding practice to have a default
-      break;
+        break;
     }
     FastLED.show();
   }
+}
+
+void processNewCommand(void)
+{
+  switch(ledCommand.command)
+  {
+    case SOLID_COLOR:
+    {
+      Serial.println(ledCommand.data[0]);
+      Serial.println(ledCommand.data[1]);
+      Serial.println(ledCommand.data[2]);
+      setCurrentPaletteRGB(ledCommand.data[0], ledCommand.data[1], ledCommand.data[2]);
+    }
+      break;
+    case CHUNKY:
+      setCurrentPalette(ledCommand.data, sizeof(ledCommand.data));
+      break;
+    default:
+      Serial.println("processCommand: Unknown command");
+      break;
+  }
+}
+
+void setCurrentPaletteRGB(byte r, byte g, byte b)
+{
+  byte bytes[8];
+  
+  bytes[0] = 0;
+  bytes[1] = r;
+  bytes[2] = g;
+  bytes[3] = b;
+  bytes[4] = 255;
+  bytes[5] = r;
+  bytes[6] = g;
+  bytes[7] = b;
+  targetPalette.loadDynamicGradientPalette(bytes);
+}
+
+void setCurrentPalette(byte* bytes, int len)
+{
+  if (len % 4 != 0)
+  {
+    Serial.println("malformed palette");
+    return;
+  }
+  targetPalette.loadDynamicGradientPalette(bytes);
 }
 
 #define TEST_PATTERN_DELAY 1000
